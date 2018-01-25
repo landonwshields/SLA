68d5964b5b8b74613cdb33417e00e6b15ba05ad1
Landon Shields <landonwshields@gmail.com>
2018-01-25 14:55:52 -0700

    styling for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,31 +8,58 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <a href='Home' onClick={this.props.goToHome}>
+        <a
+          href='Home'
+          onClick={this.props.goToHome}
+          className="navFont">
           <h4>Home</h4>
         </a>
-        <a href='About' onClick={this.props.goToAbout}>
+        <a
+          href='About'
+          onClick={this.props.goToAbout}
+          className="navFont">
           <h4>About</h4>
         </a>
-        <a href='Programs' onClick={this.props.goToPrograms}>
+        <a
+          href='Programs'
+          onClick={this.props.goToPrograms}
+          className="navFont">
           <h4>Programs</h4>
         </a>
-        <a href='Staff' onClick={this.props.goToStaff}>
+        <a
+          href='Staff'
+          onClick={this.props.goToStaff}
+          className="navFont">
           <h4>Staff</h4>
         </a>
-        <a href='Gallery' onClick={this.props.goToGallery}>
+        <a
+          href='Gallery'
+          onClick={this.props.goToGallery}
+          className="navFont" >
           <h4>Gallery</h4>
         </a>
-        <a href='News' onClick={this.props.goToNews}>
+        <a
+          href='News'
+          onClick={this.props.goToNews}
+          className="navFont" >
           <h4>News</h4>
         </a>
-        <a href='Specials' onClick={this.props.goToSpecials}>
+        <a
+          href='Specials'
+          onClick={this.props.goToSpecials}
+          className="navFont" >
           <h4>Specials</h4>
         </a>
-        <a href='Forms' onClick={this.props.goToForms}>
+        <a
+          href='Forms'
+          onClick={this.props.goToForms}
+          className="navFont" >
           <h4>Forms</h4>
         </a>
-        <a href='Contact' onClick={this.props.goToContact}>
+        <a
+          href='Contact'
+          onClick={this.props.goToContact} 
+          className="navFont" >
           <h4>Contact</h4>
         </a>
       </div>
