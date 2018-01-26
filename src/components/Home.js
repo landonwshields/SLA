eb658bf0455a794ddb2edf74d9e30cc509835b8e
Landon Shields <landonwshields@gmail.com>
2018-01-26 12:56:06 -0700

    added post function to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 export default class Home extends Component {
+  constructor(){
+    super();
+    this.state = {value: ''}
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit() {
+    fetch('https://sla-db.herokuapp.com/api/home', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        homeIntro: this.state.value
+      })
+    })
+    alert('Submitted: ' + this.state.value);
+  }
 
   render() {
     let homeStuff = this.props.data
-    // let homeInfo = function(){
-    //   for (var i = 0; i < homeStuff.length; i++) {
-    //     return homeStuff[i].homeIntro
-    //   }
-    // }
     return (
       <div className="Home">
         <h2>Home</h2>
@@ -17,8 +37,14 @@ export default class Home extends Component {
             <p>{a.homeIntro}</p>
           </div>)}
 
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              <textarea rows='7' cols='100' value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <br></br>
+            <input type="submit" value="Submit" />
+          </form>
 
-        {/* {homeStuff.length > 0 ? <h4>{homeInfo()}</h4> : null } */}
       </div>
     );
   }
