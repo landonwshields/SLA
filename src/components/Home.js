f2d264be71b296d79e33fab8dd80d1e8bd805129
Landon Shields <landonwshields@gmail.com>
2018-01-26 16:47:33 -0700

    able to update homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,21 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   constructor(){
     super();
-    this.state = {value: ''}
+    this.state = {
+      updateValue: '',
+      postValue: ''
+    }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePostChange = this.handlePostChange.bind(this);
+    this.handlePostSubmit = this.handlePostSubmit.bind(this);
+    this.handleUpdateChange = this.handleUpdateChange.bind(this);
+    this.handleUpdateSubmit = this.handleUpdateSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handlePostChange(event) {
+    this.setState({postValue: event.target.value});
   }
-
-  handleSubmit() {
+  handlePostSubmit() {
     fetch('https://sla-db.herokuapp.com/api/home', {
       method: 'POST',
       headers: {
@@ -21,11 +25,38 @@ export default class Home extends Component {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        homeIntro: this.state.value
+        homeIntro: this.state.postValue
       })
     })
-    alert('Submitted: ' + this.state.value);
+    alert('Submitted: ' + this.state.postValue);
   }
+
+  handleUpdateChange(event, id) {
+    var id = event.target.id
+    this.setState({
+      updateValue: event.target.value,
+      id: id
+    });
+    console.log(id);
+  }
+  handleUpdateSubmit(id) {
+    console.log(id)
+    var updateValue = this.state.updateValue
+    console.log(updateValue);
+    fetch('https://sla-db.herokuapp.com/api/home/' + id, {
+      method: 'PUT',
+      mode: 'CORS',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        homeIntro: updateValue
+      })
+    })
+    alert('Submitted: ' + this.state.updateValue);
+  }
+
+
 
   render() {
     let homeStuff = this.props.data
@@ -35,16 +66,21 @@ export default class Home extends Component {
         {homeStuff.map(a =>
           <div key={a.id}>
             <p>{a.homeIntro}</p>
+            <form onSubmit={() =>this.handleUpdateSubmit(a.id)} >
+              <textarea rows='7' cols='100' placeholder={a.homeIntro} id={a.id} onChange={this.handleUpdateChange}/>
+              <br></br>
+              <input type="submit" value="Update" />
+            </form>
           </div>)}
 
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handlePostSubmit}>
             <label>
-              <textarea rows='7' cols='100' value={this.state.value} onChange={this.handleChange} />
+              <textarea rows='7' cols='100' onChange={this.handlePostChange} />
             </label>
             <br></br>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Add New" />
           </form>
-
+          <br></br>
       </div>
     );
   }
