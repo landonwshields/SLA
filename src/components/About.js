db368408aa45997a5149611be055841effc75f0b
Landon Shields <landonwshields@gmail.com>
2018-01-27 16:05:30 -0700

    added admin to about page, also able to PUT and POST

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,115 @@
 import React, { Component } from 'react';
 
 export default class About extends Component {
+  constructor(){
+    super();
+    this.state = {
+      updateValue: '',
+      postValue: '',
+      active: false,
+      adminPassword: 'BadAssMom',
+      passwordDisplay: false
+    }
+
+    this.handlePostChange = this.handlePostChange.bind(this);
+    this.handlePostSubmit = this.handlePostSubmit.bind(this);
+    this.handleUpdateChange = this.handleUpdateChange.bind(this);
+    this.handleUpdateSubmit = this.handleUpdateSubmit.bind(this);
+    this.handleAdminPassword = this.handleAdminPassword.bind(this);
+    this.handleAdmin = this.handleAdmin.bind(this);
+  }
+
+  handleAdminPassword(event){
+    if (event.target.value === this.state.adminPassword) {
+      this.setState({active: true})
+    }
+  }
+
+  handleAdmin(event){
+    this.setState({passwordDisplay: true})
+  }
+
+  handlePostChange(event) {
+    this.setState({postValue: event.target.value});
+  }
+  handlePostSubmit() {
+    fetch('https://sla-db.herokuapp.com/api/about', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        aboutInfo: this.state.postValue
+      })
+    })
+    alert('Submitted: ' + this.state.postValue);
+  }
+
+  handleUpdateChange(event, id) {
+    this.setState({
+      updateValue: event.target.value
+    });
+    console.log(id);
+  }
+  handleUpdateSubmit(id) {
+    console.log(id)
+    var updateValue = this.state.updateValue
+    console.log(updateValue);
+    fetch('https://sla-db.herokuapp.com/api/about/' + id, {
+      method: 'PUT',
+      mode: 'CORS',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        aboutInfo: updateValue
+      })
+    })
+    alert('Submitted: ' + this.state.updateValue);
+  }
+
+
+
+
   render() {
     let aboutStuff = this.props.data
-    // console.log(aboutStuff);
-
     return (
       <div className="About">
-        <h2>About Us</h2>
+        <h2>About</h2>
         {aboutStuff.map(a =>
           <div key={a.id}>
             <p>{a.aboutInfo}</p>
-            <p>{a.aboutMission}</p>
-            <p>{a.aboutTeachers}</p>
+            {
+              this.state.active ?
+              <form onSubmit={() =>this.handleUpdateSubmit(a.id)} >
+                <textarea rows='7' cols='100' placeholder={a.aboutInfo} id={a.id} onChange={this.handleUpdateChange}/>
+                <br></br>
+                <input type="submit" value="Update" />
+              </form> : null
+            }
           </div>)}
+        <br></br>
+          {
+            this.state.active ?
+            <form onSubmit={this.handlePostSubmit}>
+              <label>
+                <textarea rows='7' cols='100' onChange={this.handlePostChange} />
+              </label>
+              <br></br>
+              <input type="submit" value="Add New" />
+            </form> : null
+          }
+          <br></br>
+          <button onClick={this.handleAdmin}>Admin</button>
+          {
+            this.state.passwordDisplay ?
+            <form>
+              <textarea rows='1' cols='15' onChange={this.handleAdminPassword} />
+              <br></br>
+              {/* <input type="submit" value="Admin Password" /> */}
+            </form> : null
+          }
       </div>
     );
   }
